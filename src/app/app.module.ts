92a5182096fcf4077ca73e4beff8f3377243eb68
Gowtham Kumar Rodda <gowtham@olivetech.net>
2018-03-25 03:27:22 +0530

    adds carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule}      from '@angular/core';
+import {BrowserModule}     from '@angular/platform-browser';
+import {NgModule}          from '@angular/core';
+import {NgxCarouselModule} from 'ngx-carousel';
 
 import {AppComponent}        from './app.component';
 import {IbingMaterialModule} from './ibing-material/ibing-material.module';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AboutComponent}   from './about/about.component';
-import { HeaderComponent } from './shared/components/header/header.component';
+import {AppRoutingModule}  from './app-routing.module';
+import {AboutComponent}    from './about/about.component';
+import {HeaderComponent}   from './shared/components/header/header.component';
+import {CarouselComponent} from './carousel/carousel.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     IbingMaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
